266913ec28bdf5c6ee4ef7917ff25e2a42ff47b0
Krasty <krasty_90@mail.ru>
2020-06-17 16:33:45 +0300

    add2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 -PHP-JAVASCRIPT-JQUERY-AJAX
+
+Cookie doesn't work correctly. If u go back to the page u can see the last sesion but u cannot correct smth. U can add another todo list and remove the last one but u cannot delete the first input of the todo list. 85/100 
---
+++ ex03/ex02bis/todo.js
@@ -0,0 +1,49 @@
+var tab = [];
+var i = 0;
+var c;
+
+function set_cookie()
+{
+    document.cookie = "qwe=" + escape("#ft_list".innerHTML)+'; max-age=86400;';
+}
+
+function del_l(c)
+{
+    if (confirm("Are u sure to delete"))
+    {   
+        c.remove();
+        set_cookie();
+    }
+}
+
+function add_new()
+{
+  var todo = prompt(": ");
+  if (todo != "" && todo !== null)
+  {
+    var newitem = $('#ft_list')[0];
+    var newdiv = $("<div></div>")[0];
+    newdiv.innerHtml = todo;
+    var textnode = document.createTextNode(todo);
+    newdiv.onclick = function (){del_l(this);};
+    newdiv.appendChild(textnode);
+    newitem.insertBefore(newdiv, newitem.firstChild);
+    set_cookie(); 
+  }
+}
+
+function getCookies()
+{
+	var cookiesArray = document.cookie.split(';');
+	var array = {};
+
+	for (var x in cookiesArray)
+	{
+		var toto = cookiesArray[x].split("=");
+        if (toto[0] == "qwe")
+            break;
+    }
+    var newitem = $('#ft_list')[0];
+    if (toto[1] !== undefined)
+		    newitem.innerHTML = unescape(toto[1]);    
+}
